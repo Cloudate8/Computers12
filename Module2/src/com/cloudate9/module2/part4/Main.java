6fb6bd765a9a35679fe1c0ddfbea000665905998
Cloudate9 <65006925+Cloudate9@users.noreply.github.com>
2023-11-03 13:41:46 -0700

    Complete 2.4

---
+++ Module2/src/com/cloudate9/module2/part4/Main.java
@@ -0,0 +1,23 @@
+package com.cloudate9.module2.part4;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        // illiad.txt is put in the root folder for ease of access
+        Set<String> unique = new HashSet<>();
+        Scanner scanner = new Scanner(new File("illiad.txt"));
+        while (scanner.hasNext()) {
+            // Make word lowercase and filter out all non-alphabetic characters
+            String word = scanner.next().toLowerCase().replaceAll("[^a-z]", "");
+            unique.add(word);
+        }
+        scanner.close();
+        System.out.println("Number of unique words: " + unique.size());
+    }
+}
